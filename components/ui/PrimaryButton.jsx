bcac5d8723338a0a8c561b8d9f8797ff02f30632
verseler <handuman.verselerkerr@gmail.com>
2024-06-20 20:44:29 +0800

    feat: add primaryButton component

---
+++ components/ui/PrimaryButton.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import { useTheme } from "react-native-paper";
+
+const PrimaryButton = (props) => {
+  const theme = useTheme();
+
+  const { label, onPress, ...buttonProps } = props;
+
+  return (
+    <Button
+      mode="contained"
+      onPress={onPress}
+      contentStyle={{ padding: theme.padding.button.base }}
+      labelStyle={{ fontSize: theme.fontSize.base }}
+      style={{ borderRadius: theme.borderRadius.base }}
+      {...buttonProps}
+    >
+      {label}
+    </Button>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({});
