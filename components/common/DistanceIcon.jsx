3d4c7b85d2edbef15ee21c682e0d84d887ee0af3
verseler <handuman.verselerkerr@gmail.com>
2024-07-02 21:36:04 +0800

    feat: disanceIcon component for emergencyAlertList

---
+++ components/common/DistanceIcon.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, View } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+import { Feather } from "@expo/vector-icons";
+
+const DistanceIcon = ({ distance, status }) => {
+  const theme = useTheme();
+
+  const iconColor = status ? theme.colors.green : theme.colors.primary;
+
+  return (
+    <View style={styles.container}>
+      <Feather name="map-pin" size={28} color={iconColor} />
+      <Text
+        style={[styles.distance, { color: theme.colors.typography.secondary }]}
+      >
+        {distance}
+      </Text>
+    </View>
+  );
+};
+
+export default DistanceIcon;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    rowGap: 4,
+  },
+  distance: {
+    fontSize: 9,
+  },
+});
