aec20238ce56c350e352a8f7411387cd852189cd
Amawers <dacoroon.christianjericho@gmail.com>
2024-07-08 21:21:40 +0800

    feat(profile): realt time event listener

--- App.js
+++ App.js
@@ -1,34 +1,80 @@
-import "expo-dev-client";
-import "react-native-gesture-handler";
-import { PaperProvider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useEffect } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import 'expo-dev-client'
+import 'react-native-gesture-handler'
+import { PaperProvider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { lightTheme, darkTheme, themeStatus } from './utils/theme'
+import { SignedInStack, SignedOutStack } from './navigation/ScreenStack'
+import CircularIcon from './components/ui/CircularIcon'
+import useBoundStore from './zustand/useBoundStore'
+import { supabase } from './utils/supabase/config'
 
-import { lightTheme, darkTheme, themeStatus } from "./utils/theme";
-import { SignedInStack, SignedOutStack } from "./navigation/ScreenStack";
-import CircularIcon from "./components/ui/CircularIcon";
-import useBoundStore from "./zustand/useBoundStore";
-
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const currentThemeStatus = useBoundStore((state) => state.currentThemeStatus);
-  const setThemeStatus = useBoundStore((state) => state.setThemeStatus);
+  const currentThemeStatus = useBoundStore((state) => state.currentThemeStatus)
+  const setThemeStatus = useBoundStore((state) => state.setThemeStatus)
   const selectedTheme =
-    currentThemeStatus == themeStatus.light ? lightTheme : darkTheme;
-  const globalStateEncryptedSession = useBoundStore((state) => state.session);
+    currentThemeStatus == themeStatus.light ? lightTheme : darkTheme
+  const globalStateEncryptedSession = useBoundStore((state) => state.session)
+  const setUserMetaData = useBoundStore((state) => state.setUserMetaData)
+
+  // const { data } = supabase.auth.onAuthStateChange((event, session) => {
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (event === 'INITIAL_SESSION') {
+      //! handle initial session
+      try {
+        setUserMetaData({
+          firstName: session['user']['user_metadata']['first_name'],
+          middleName: session['user']['user_metadata']['middle_name'],
+          lastName: session['user']['user_metadata']['last_name'],
+          suffix: session['user']['user_metadata']['suffix'],
+          birthday: session['user']['user_metadata']['birth_date'],
+          phone: session['user']['user_metadata']['phone_number'],
+          barangay: session['user']['user_metadata']['barangay'],
+          street: session['user']['user_metadata']['street'],
+          houseNumber: session['user']['user_metadata']['house_number'],
+          email: session['user']['user_metadata']['email'],
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    } else if (event === 'USER_UPDATED') {
+      //! handle initial session
+      try {
+        setUserMetaData({
+          firstName: session['user']['user_metadata']['first_name'],
+          middleName: session['user']['user_metadata']['middle_name'],
+          lastName: session['user']['user_metadata']['last_name'],
+          suffix: session['user']['user_metadata']['suffix'],
+          birthday: session['user']['user_metadata']['birth_date'],
+          phone: session['user']['user_metadata']['phone_number'],
+          barangay: session['user']['user_metadata']['barangay'],
+          street: session['user']['user_metadata']['street'],
+          houseNumber: session['user']['user_metadata']['house_number'],
+          email: session['user']['user_metadata']['email'],
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+
+  // // call unsubscribe to remove the callback
+  // data.subscription.unsubscribe()
 
   useEffect(() => {
     const initThemeCheck = async () => {
-      setThemeStatus(await AsyncStorage.getItem("theme"));
-    };
+      setThemeStatus(await AsyncStorage.getItem('theme'))
+    }
 
-    initThemeCheck();
-  }, []);
+    initThemeCheck()
+  }, [])
 
   /*
    *
@@ -39,14 +85,14 @@ export default function App() {
 
   // Add a default header to all screens
   const screenOptions = ({ navigation }) => ({
-    presentation: "containedTransparentModal",
-    animation: "fade",
+    presentation: 'containedTransparentModal',
+    animation: 'fade',
     contentStyle: { backgroundColor: selectedTheme.colors.background },
     headerStyle: { backgroundColor: selectedTheme.colors.background },
     headerShadowVisible: false,
-    headerTitleAlign: "center",
+    headerTitleAlign: 'center',
     headerTitleStyle: {
-      fontWeight: "bold",
+      fontWeight: 'bold',
       color: selectedTheme.colors.typography.primary,
     },
     headerLeft: () => (
@@ -56,7 +102,7 @@ export default function App() {
         onPress={() => navigation.goBack()}
       />
     ),
-  });
+  })
 
   return (
     <GestureHandlerRootView>
@@ -70,5 +116,5 @@ export default function App() {
         </PaperProvider>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
-  );
+  )
 }
