d2e08467f054393e5fa1c2c2b34d2598ee64afc6
Amawers <dacoroon.christianjericho@gmail.com>
2024-07-10 20:06:10 +0800

    feat(profile): added date formatter for birhtday and initial state values

--- screen/profile/myAccount/MyAccountScreen.jsx
+++ screen/profile/myAccount/MyAccountScreen.jsx
@@ -1,48 +1,62 @@
-import { SectionList, StyleSheet } from "react-native";
-import { Divider, useTheme } from "react-native-paper";
-
-import UserProfileCard from "../../../components/profile/UserProfileCard";
-import SectionHeader from "../../../components/profile/SectionHeader";
-import SectionItem from "../../../components/profile/SectionItem";
-import EditButton from "../../../components/profile/EditButton";
-import StatusBar from "../../../components/common/StatusBar";
-
-const DATA = [
-  {
-    title: "Personal Information",
-    data: [
-      { label: "First Name", value: "John" },
-      { label: "Middle Name", value: "-" },
-      { label: "Last Name", value: "Doe" },
-      { label: "Suffix", value: "-" },
-      { label: "Birthday", value: "20 Jan 2022" },
-      { label: "Phone", value: "091231123123" },
-    ],
-  },
-  {
-    title: "Address",
-    data: [
-      { label: "Barangay", value: "Oakwood Court" },
-      { label: "Street", value: "Block 16, Zone 3" },
-      { label: "House Number", value: "28" },
-    ],
-  },
-];
+import { SectionList, StyleSheet } from 'react-native'
+import { Divider, useTheme } from 'react-native-paper'
+import UserProfileCard from '../../../components/profile/UserProfileCard'
+import SectionHeader from '../../../components/profile/SectionHeader'
+import SectionItem from '../../../components/profile/SectionItem'
+import EditButton from '../../../components/profile/EditButton'
+import StatusBar from '../../../components/common/StatusBar'
+import useBoundStore from '../../../zustand/useBoundStore'
 
 const MyAccountScreen = () => {
-  const theme = useTheme();
+  const theme = useTheme()
+  const userMetaData = useBoundStore((state) => state.userMetaData)
+
+  //! format date to string (ex.july 1, 2024)
+  const formatDate = (date) => {
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    })
+  }
+
+  const date = new Date(userMetaData['birthday'])
+  const formattedDate = formatDate(date)
+
+  //! array template for UI rendering
+  const DATA = [
+    {
+      title: 'Personal Information',
+      data: [
+        { label: 'First Name', value: userMetaData['firstName'] },
+        { label: 'Middle Name', value: userMetaData['middleName'] },
+        { label: 'Last Name', value: userMetaData['lastName'] },
+        { label: 'Suffix', value: userMetaData['suffix'] },
+        { label: 'Birthday', value: formattedDate },
+        { label: 'Phone', value: userMetaData['phone'] },
+      ],
+    },
+    {
+      title: 'Address',
+      data: [
+        { label: 'Barangay', value: userMetaData['barangay'] },
+        { label: 'Street', value: userMetaData['street'] },
+        { label: 'House Number', value: userMetaData['houseNumber'] },
+      ],
+    },
+  ]
 
   const renderItem = ({ item }) => (
     <SectionItem label={item.label} value={item.value} />
-  );
+  )
 
   const renderSectionHeader = ({ section: { title } }) => (
     <SectionHeader title={title} />
-  );
+  )
 
   const renderItemSeperator = () => (
     <Divider style={{ marginHorizontal: theme.padding.body.horizontal }} />
-  );
+  )
 
   return (
     <>
@@ -54,8 +68,8 @@ const MyAccountScreen = () => {
         ListHeaderComponentStyle={styles.listHeaderContainer}
         ListHeaderComponent={
           <UserProfileCard
-            name="John Doe"
-            email="j.doe@gmail.com"
+            name={`${userMetaData['firstName']} ${userMetaData['middleName']} ${userMetaData['lastName']} ${userMetaData['suffix']}`}
+            email={userMetaData['email']}
             renderFooter={() => <EditButton />}
           />
         }
@@ -65,13 +79,13 @@ const MyAccountScreen = () => {
 
       <StatusBar />
     </>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   listHeaderContainer: {
     marginBottom: 16,
   },
-});
+})
 
-export default MyAccountScreen;
+export default MyAccountScreen
