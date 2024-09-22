f9a23f15012de722adc2498228c61b0aefbefdf4
Amawers <dacoroon.christianjericho@gmail.com>
2024-09-22 23:59:19 +0800

    feat(verification): global picture state holder
    added custom individual base64 formatter for front and back ID and global state path holder for both

--- zustand/utilSlice.jsx
+++ zustand/utilSlice.jsx
@@ -1,4 +1,8 @@
 export const createUtilSlice = (set) => ({
   base64ImageFormat: null,
+  verificationIdOneBase64: null,
+  verificationIdTwoBase64: null,
   setBase64ImageFormat: (state) => set({ base64ImageFormat: state }),
+  setBase64VerOne: (state) => set({ verificationIdOneBase64: state }),
+  setBase64VerTwo: (state) => set({ verificationIdTwoBase64: state })
 })
