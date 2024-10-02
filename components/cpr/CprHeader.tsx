7a7d260b78f40405c07375f23d3c652c58e91913
verseler <handuman.verselerkerr@gmail.com>
2024-10-02 17:31:37 +0800

    style: change menu background color

--- components/cpr/CprHeader.tsx
+++ components/cpr/CprHeader.tsx
@@ -2,19 +2,20 @@ import { View, StyleSheet } from "react-native";
 import { useState, memo } from "react";
 import { Divider, Menu, TouchableRipple } from "react-native-paper";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MenuItem from "react-native-paper/lib/typescript/components/Menu/MenuItem";
+import { createStyleSheet, useStyles } from "../../hooks/useStyles";
+import { MD3Theme } from "react-native-paper";
 
 type CprHeaderProps = {
   handleEnd: () => void;
 };
 
-function Header({  handleEnd }: CprHeaderProps) {
+function Header({ handleEnd }: CprHeaderProps) {
+  const { styles } = useStyles(stylesheet);
   const [visibleActionMenu, setVisibleActionMenu] = useState(false);
-
 
   const openActionMenu = (): void => setVisibleActionMenu(true);
   const closeActionMenu = (): void => setVisibleActionMenu(false);
-
-  
 
   return (
     <View style={styles.header}>
@@ -22,8 +23,8 @@ function Header({  handleEnd }: CprHeaderProps) {
         visible={visibleActionMenu}
         onDismiss={closeActionMenu}
         anchor={<MoreOptionActionButton onPress={openActionMenu} />}
+        contentStyle={styles.menu}
       >
-        <Divider />
         <Menu.Item onPress={handleEnd} title="End" />
       </Menu>
     </View>
@@ -35,8 +36,10 @@ type MoreOptionActionButtonProps = {
 };
 
 function MoreOptionActionButton({ onPress }: MoreOptionActionButtonProps) {
+  const { styles } = useStyles(stylesheet);
+
   return (
-    <TouchableRipple onPress={onPress} style={styles.optionButton}>
+    <TouchableRipple borderless onPress={onPress} style={styles.optionButton}>
       <MaterialCommunityIcons name="dots-vertical" size={24} color="black" />
     </TouchableRipple>
   );
@@ -44,20 +47,25 @@ function MoreOptionActionButton({ onPress }: MoreOptionActionButtonProps) {
 
 export const CprHeader = memo(Header);
 
-const styles = StyleSheet.create({
-  header: {
-    height: 50,
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 10,
-    justifyContent: "flex-end",
-  },
-  optionButton: {
-    width: 33,
-    height: 33,
-    borderRadius: 33,
-    backgroundColor: "lightgray",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const stylesheet = createStyleSheet((theme: MD3Theme) =>
+  StyleSheet.create({
+    header: {
+      height: 50,
+      flexDirection: "row",
+      alignItems: "center",
+      paddingHorizontal: 10,
+      justifyContent: "flex-end",
+    },
+    menu: {
+      backgroundColor: theme.colors.elevation.level3,
+    },
+    optionButton: {
+      width: 33,
+      height: 33,
+      borderRadius: 33,
+      backgroundColor: "lightgray",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  })
+);
