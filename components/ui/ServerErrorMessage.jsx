8eb398826bd28c53fd48068784a8eee731206493
verseler <handuman.verselerkerr@gmail.com>
2024-10-17 18:28:19 +0800

    style: hide serverErrorMessage if it has no content

--- components/ui/ServerErrorMessage.jsx
+++ components/ui/ServerErrorMessage.jsx
@@ -3,6 +3,9 @@ import { createStyleSheet, useStyles } from "../../hooks/useStyles";
 
 export default function ServerErrorMessage({ style, children, ...props }) {
   const { styles } = useStyles(stylesheet);
+
+  if (!children) return;
+
   return (
     <Text style={[styles.serverErrorMessage, style]} {...props}>
       {children}
