b305ddf24ac0ffa983b071f2be6a8623e78be169
verseler <handuman.verselerkerr@gmail.com>
2024-11-11 07:35:54 +0800

    refactor: change questions and answer

--- screen/learn/QuizScreen.jsx
+++ screen/learn/QuizScreen.jsx
@@ -28,7 +28,7 @@ const QuizScreen = ({ route }) => {
   //!temp default value
   //!diri ibutang ang na fetched na quiz
   const [quiz, setQuiz] = useState(
-    QUIZ_QUESTION_SAMPLE.find((quiz) => quiz.id == id).questions
+    QUIZ_QUESTION.find((quiz) => quiz.id == id).questions
   );
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const currentQuestion = quiz[currentQuestionIndex];
@@ -241,111 +241,171 @@ const stylesheet = createStyleSheet((theme) => ({
   },
 }));
 
-//!temp
-const QUIZ_QUESTION_SAMPLE = [
+const QUIZ_QUESTION = [
   {
     id: 1,
     questions: [
       {
         question:
+          "What is the first step in performing CPR when approaching the scene?",
+        options: [
+          { id: 1, text: "Call 9-1-1" },
+          { id: 2, text: "Check for breathing" },
+          { id: 3, text: "Check the scene for safety" },
+          { id: 4, text: "Begin chest compressions" },
+        ],
+        correctAnswerOption: 3,
+      },
+      {
+        question:
+          "How do you check for responsiveness in a person who appears unresponsive?",
+        options: [
+          { id: 1, text: "Tap on the person's hand" },
+          { id: 2, text: "Shout and shake the person" },
+          { id: 3, text: "Shout-tap-shout" },
+          { id: 4, text: "Start chest compressions immediately" },
+        ],
+        correctAnswerOption: 3,
+      },
+      {
+        question:
           "What should you do if the person is unresponsive and not breathing or only gasping?",
         options: [
-          { id: 1, text: "Perform CPR" },
-          { id: 2, text: "Wait for help" },
-          { id: 3, text: "Give them water" },
-          { id: 4, text: "Check their pulse" },
+          { id: 1, text: "Call 9-1-1" },
+          { id: 2, text: "Begin chest compressions immediately" },
+          { id: 3, text: "Try to wake them by shaking" },
+          { id: 4, text: "Wait for someone else to help" },
         ],
         correctAnswerOption: 1,
       },
       {
-        question: "Which is the primary organ for respiration?",
+        question:
+          "Where should you place your hands when giving chest compressions?",
         options: [
-          { id: 1, text: "Heart" },
-          { id: 2, text: "Lungs" },
-          { id: 3, text: "Brain" },
-          { id: 4, text: "Tail" },
+          { id: 1, text: "On the lower abdomen" },
+          { id: 2, text: "On the person's neck" },
+          { id: 3, text: "On the person's back" },
+          { id: 4, text: "Two hands centered on the chest" },
         ],
-        correctAnswerOption: 2,
+        correctAnswerOption: 4,
       },
       {
-        question: "What is the chemical symbol for water?",
+        question:
+          " What is the recommended rate of chest compressions per minute??",
         options: [
-          { id: 1, text: "O2" },
-          { id: 2, text: "H2O" },
-          { id: 3, text: "CO2" },
-          { id: 4, text: "HO2" },
-        ],
-        correctAnswerOption: 2,
-      },
-      {
-        question: "Which planet is known as the Red Planet?",
-        options: [
-          { id: 1, text: "Earth" },
-          { id: 2, text: "Mars" },
-          { id: 3, text: "Jupiter" },
-          { id: 4, text: "Saturn" },
-        ],
-        correctAnswerOption: 2,
-      },
-      {
-        question: "What is the capital of France?",
-        options: [
-          { id: 1, text: "Berlin" },
-          { id: 2, text: "Rome" },
-          { id: 3, text: "Paris" },
-          { id: 4, text: "Madrid" },
+          { id: 1, text: "50-70 compressions per minute" },
+          { id: 2, text: "90-110 compressions per minute" },
+          { id: 3, text: "100-120 compressions per minute" },
+          { id: 4, text: "130-150 compressions per minute" },
         ],
         correctAnswerOption: 3,
       },
       {
-        question: "What is the largest bone in the human body?",
+        question: "What depth should chest compressions be during CPR?",
         options: [
-          { id: 1, text: "Tibia" },
-          { id: 2, text: "Femur" },
-          { id: 3, text: "Fibula" },
-          { id: 4, text: "Humerus" },
+          { id: 1, text: "At least 1 inch" },
+          { id: 2, text: "At least 2 inches" },
+          { id: 3, text: "1.5 inches" },
+          { id: 4, text: "3 inches" },
         ],
         correctAnswerOption: 2,
       },
       {
-        question: "Which element is necessary for human bones to be strong?",
+        question:
+          "How many chest compressions should you give before providing breaths?",
         options: [
-          { id: 1, text: "Iron" },
-          { id: 2, text: "Calcium" },
-          { id: 3, text: "Potassium" },
-          { id: 4, text: "Carbon" },
-        ],
-        correctAnswerOption: 2,
-      },
-      {
-        question: "Which ocean is the largest in the world?",
-        options: [
-          { id: 1, text: "Atlantic Ocean" },
-          { id: 2, text: "Indian Ocean" },
-          { id: 3, text: "Pacific Ocean" },
-          { id: 4, text: "Arctic Ocean" },
+          { id: 1, text: "20" },
+          { id: 2, text: "25" },
+          { id: 3, text: "30" },
+          { id: 4, text: "35" },
         ],
         correctAnswerOption: 3,
       },
       {
-        question: "What gas do plants absorb from the atmosphere?",
+        question:
+          "When giving breaths during CPR, how should you open the airway?",
         options: [
-          { id: 1, text: "Oxygen" },
-          { id: 2, text: "Carbon Dioxide" },
-          { id: 3, text: "Nitrogen" },
-          { id: 4, text: "Helium" },
+          { id: 1, text: "Push the head back without moving the chin" },
+          { id: 2, text: "Tilt the head back and lift the chin" },
+          { id: 3, text: "Use both hands to press on the chest" },
+          { id: 4, text: "Blow into the person's nose only" },
         ],
         correctAnswerOption: 2,
       },
       {
-        question: "Which continent is the Sahara Desert located in?",
+        question:
+          "What should you do if the first breath does not cause the chest to rise?",
         options: [
-          { id: 1, text: "Asia" },
-          { id: 2, text: "Africa" },
-          { id: 3, text: "Australia" },
-          { id: 4, text: "North America" },
+          { id: 1, text: "Try a third breath immediately" },
+          { id: 2, text: "Retilt the head and ensure a proper seal" },
+          { id: 3, text: "Give up and start compressions again" },
+          { id: 4, text: "Remove all PPE and retry" },
         ],
         correctAnswerOption: 2,
+      },
+      {
+        question:
+          "What should you do after completing 30 chest compressions during CPR?",
+        options: [
+          { id: 1, text: "Give 2 rescue breaths" },
+          { id: 2, text: "Stop and wait for help to arrive" },
+          { id: 3, text: "Give 5 more compressions" },
+          { id: 4, text: "Check the person's pulse" },
+        ],
+        correctAnswerOption: 1,
+      },
+      {
+        question:
+          "What is the primary goal of the SIPIAR CPR mobile application?",
+        options: [
+          { id: 1, text: "To teach advanced medical procedures" },
+          { id: 2, text: "To help bystanders perform CPR " },
+          { id: 3, text: "To replace professional emergency services" },
+          { id: 4, text: "To provide general health advice" },
+        ],
+        correctAnswerOption: 2,
+      },
+      {
+        question: "How does the app guide a user through the CPR process?",
+        options: [
+          { id: 1, text: "Audio and visual guides" },
+          { id: 2, text: " Video tutorials without audio" },
+          { id: 3, text: "Text instructions only" },
+          { id: 4, text: "Animation without instructions" },
+        ],
+        correctAnswerOption: 1,
+      },
+      {
+        question:
+          "What additional function does SIPIAR provide beyond CPR instructions?",
+        options: [
+          { id: 1, text: "Connects users to emergency dispatchers" },
+          { id: 2, text: "Provides medical history of the patient" },
+          { id: 3, text: "Tracks the user’s CPR performance for feedback" },
+          { id: 4, text: "Offers a list of nearby hospitals" },
+        ],
+        correctAnswerOption: 3,
+      },
+      {
+        question: "What age group does the SIPIAR app focus on for CPR?",
+        options: [
+          { id: 1, text: "Infants" },
+          { id: 2, text: "Children" },
+          { id: 3, text: "All age groups" },
+          { id: 4, text: "Adults only" },
+        ],
+        correctAnswerOption: 4,
+      },
+      {
+        question:
+          "Which of the following does the SIPIAR app NOT provide assistance with?",
+        options: [
+          { id: 1, text: "CPR for infants and children" },
+          { id: 2, text: "Monitoring compression depth during CPR" },
+          { id: 3, text: " CPR for adults with cardiac arrest" },
+          { id: 4, text: "Timing chest compressions" },
+        ],
+        correctAnswerOption: 1,
       },
     ],
   },
