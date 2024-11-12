1489ec9e2612b5e2fc6705ff615d2f106eedd496
verseler <handuman.verselerkerr@gmail.com>
2024-11-12 19:52:37 +0800

    feat: add image picker field for image of the issue

---
+++ components/profile/settings/ReportImageFrame.jsx
@@ -0,0 +1,76 @@
+import { Image, View, Text } from "react-native";
+import { TouchableRipple } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
+
+import { createStyleSheet, useStyles } from "../../../hooks/useStyles";
+import useImagePicker from "../../../hooks/useImagePicker";
+
+const ReportImageFrame = ({ label, image, setImage, isLoading, error }) => {
+  const { styles, theme } = useStyles(stylesheet);
+  const { pickImage } = useImagePicker();
+
+  return (
+    <View>
+      <Text style={styles.label}>{label}</Text>
+      <TouchableRipple
+        onPress={() => pickImage(setImage)}
+        disabled={isLoading}
+        style={{ width: "100%" }}
+      >
+        <View
+          style={[
+            styles.imageFrame,
+            { borderColor: error ? theme.colors.primary : "#e1e2e3" },
+          ]}
+        >
+          {image ? (
+            <Image source={{ uri: image }} style={styles.image} />
+          ) : (
+            <>
+              <Ionicons name="camera" size={50} color={theme.colors.text3} />
+              <Text style={styles.imagePlaceholder}>Upload a picture</Text>
+            </>
+          )}
+        </View>
+      </TouchableRipple>
+
+      {error && (
+        <Text style={[styles.errorLabel, { color: theme.colors.primary }]}>
+          {error}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+export default ReportImageFrame;
+
+const stylesheet = createStyleSheet((theme) => ({
+  label: {
+    color: theme.colors.text,
+    fontWeight: "500",
+    marginBottom: 8,
+  },
+  imageFrame: {
+    borderWidth: 1,
+    borderRadius: 10,
+    width: "100%",
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    objectFit: "contain",
+  },
+  errorLabel: {
+    paddingStart: 14,
+    paddingTop: 4,
+    marginBottom: 4,
+  },
+  imagePlaceholder: {
+    color: theme.colors.text3,
+  },
+}));
