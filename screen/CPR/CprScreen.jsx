e8762819bfe700f2bcb26c3edf1484c50c51d1f8
Verseler kerr Handuman <121875436+Verseler@users.noreply.github.com>
2024-11-28 22:05:33 +0800

    feat: show CprGuideInfo for the first time

--- screen/CPR/CprScreen.jsx
+++ screen/CPR/CprScreen.jsx
@@ -12,9 +12,16 @@ import Countdown from "../../components/cpr/Countdown";
 import useCpr from "../../hooks/cpr/useCpr";
 import CprInfoDialog from "../../components/cpr/CprInfoDialog";
 import usePreventBack from "../../hooks/usePreventBack";
+import useFirstTimePopup from "../../hooks/useFirstTimePopup";
 
 function CprScreen() {
   usePreventBack();
+  const { markAsDone } = useFirstTimePopup({
+    key: "CprGuideInfo",
+    handleFalse: () => {
+      setIsInfoDialogVisible(true), markAsDone();
+    },
+  });
   const {
     timer,
     start: startCpr,
