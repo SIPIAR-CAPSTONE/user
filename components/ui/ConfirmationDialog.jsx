02dc30fbac7fdea40656cc02bfb41aef6512b420
Verseler kerr Handuman <121875436+Verseler@users.noreply.github.com>
2024-11-28 22:42:44 +0800

    style: add logout loading effects

--- components/ui/ConfirmationDialog.jsx
+++ components/ui/ConfirmationDialog.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import Button from "./Button";
 import { createStyleSheet, useStyles } from "../../hooks/useStyles";
-import { View } from "react-native";
 
 const ConfirmationDialog = ({
   isVisible,
