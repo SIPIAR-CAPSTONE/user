6644ade2c65d374a54358a269af6a286937e4291
Verseler kerr Handuman <121875436+Verseler@users.noreply.github.com>
2024-11-30 00:11:01 +0800

    refactor: move formatTime to its respected helper file

--- hooks/cpr/useCpr.helper.js
+++ hooks/cpr/useCpr.helper.js
@@ -74,14 +74,3 @@ export const getOverallScore = (timingScore, depthScore) => {
 
   return "gray";
 };
-
-export const formatTime = (time) => {
-  const totalSeconds = Math.floor(time / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-  const formattedTime = `${minutes < 10 ? "0" : ""}${minutes}:${
-    seconds < 10 ? "0" : ""
-  }${seconds}`;
-
-  return formattedTime;
-};
