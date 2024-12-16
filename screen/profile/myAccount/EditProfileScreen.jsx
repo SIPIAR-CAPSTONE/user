fb40ded7e3dadadb70d5fb36c2c5b4ab621d4e10
Verseler kerr Handuman <121875436+Verseler@users.noreply.github.com>
2024-12-17 03:13:41 +0800

    fix: phone input type

--- screen/profile/myAccount/EditProfileScreen.jsx
+++ screen/profile/myAccount/EditProfileScreen.jsx
@@ -218,6 +218,7 @@ const EditProfileScreen = () => {
         <TextInput
           variant="outlined"
           label="Phone"
+          type="numeric"
           value={userInfo.phone}
           onChangeText={(item) => handleFieldChange("phone", item)}
         />
